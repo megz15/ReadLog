0bca89ccc185970babc0032b4cd3426063082f44
megz15 <meghraj.g16@gmail.com>
2023-06-14 02:08:00 +0530

    reflect those changes in screens

--- lib/screens/book/book_general.dart
+++ lib/screens/book/book_general.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:megz_readlog/providers.dart';
 
-class BookGeneral extends StatelessWidget {
+class BookGeneral extends ConsumerWidget {
   const BookGeneral({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<String> wordList = ref.watch(wordListProvider);
+
+    return ListView.builder(
+        itemCount: wordList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: const Icon(Icons.book),
+            title: Text(wordList[index]),
+            trailing: IconButton(
+              onPressed: () {
+                ref.read(wordListProvider.notifier).state = wordList
+                    .where((x) => wordList.indexOf(x) != index)
+                    .toList();
+              },
+              icon: const Icon(Icons.delete),
+            ),
+          );
+        });
   }
 }
